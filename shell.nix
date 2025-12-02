e7178311c591e184bfac8cdaf2b8af99339aa1b9
Blaise Le Coquil <blaiselcq@blaiselcq.net>
2025-12-02 06:55:39 +0100

    Add Hyperfine to the shell

--- shell.nix
+++ shell.nix
@@ -3,6 +3,14 @@ let pkgs = import <nixpkgs> { };
 in pkgs.mkShell {
   name = "avc";
 
-  buildInputs = with pkgs; [ cargo clippy docopts rust-analyzer rustc rustfmt ];
+  buildInputs = with pkgs; [
+    cargo
+    clippy
+    docopts
+    rust-analyzer
+    rustc
+    rustfmt
+    hyperfine
+  ];
 }
 
