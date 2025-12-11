5dbf4be1bb30c8e404c6a9916f39092524ead74e
Blaise Le Coquil <blaiselcq@blaiselcq.net>
2025-12-11 09:24:03 +0100

    2025 day 10 part 2 wip

--- rust/src/days/y2025/day_10.rs
+++ rust/src/days/y2025/day_10.rs
@@ -1,3 +1,4 @@
+use rayon::prelude::*;
 use std::collections::BTreeMap;
 
 #[derive(Debug)]
@@ -47,7 +48,7 @@ fn load_input(input: &str) -> Vec<Machine> {
         .collect()
 }
 
-fn press_buttons(mut lights: Vec<bool>, button: &[usize]) -> Vec<bool> {
+fn press_buttons_for_lights(mut lights: Vec<bool>, button: &[usize]) -> Vec<bool> {
     for &i in button {
         lights[i] = !lights[i];
     }
@@ -55,7 +56,15 @@ fn press_buttons(mut lights: Vec<bool>, button: &[usize]) -> Vec<bool> {
     lights
 }
 
-fn search_buttons_press(machine: &Machine) -> usize {
+fn press_buttons_for_joltage(mut joltage: Vec<u32>, button: &[usize], presses: u32) -> Vec<u32> {
+    for &i in button {
+        joltage[i] += presses;
+    }
+
+    joltage
+}
+
+fn search_buttons_for_lights(machine: &Machine) -> usize {
     let mut states = BTreeMap::from([(0usize, vec![vec![false; machine.lights.len()]])]);
     let min_presses = loop {
         let mut entry = states.first_entry().unwrap();
@@ -75,8 +84,53 @@ fn search_buttons_press(machine: &Machine) -> usize {
             machine
                 .buttons
                 .iter()
-                .map(|button| press_buttons(state.clone(), button)),
+                .map(|button| press_buttons_for_lights(state.clone(), button)),
         );
+    };
+
+    min_presses
+}
+
+fn joltage_all_buttons(machine: &Machine) -> Vec<u32> {
+    let mut joltage = vec![0; machine.joltage.len()];
+    for button in &machine.buttons {
+        joltage = press_buttons_for_joltage(joltage, button, 1);
+    }
+    joltage
+}
+
+fn search_buttons_for_joltage(machine: &Machine) -> u32 {
+    let total_joltage: u32 = machine.joltage.iter().sum();
+
+    let mut states = BTreeMap::from([(0u32, vec![(0u32, vec![0; machine.joltage.len()])])]);
+
+    let range = (0..=machine.joltage.iter().max().unwrap().ilog2()).map(|n| 2u32.pow(n));
+    let min_presses = loop {
+        let mut entry = states.first_entry().unwrap();
+        let cost = *entry.key();
+        let (presses, state) = entry.get_mut().pop().unwrap();
+
+        if entry.get().is_empty() {
+            states.remove(&cost);
+        }
+
+        if state == machine.joltage {
+            break presses;
+        }
+
+        for button in &machine.buttons {
+            for p in range.clone() {
+                let new_joltage = press_buttons_for_joltage(state.clone(), button, p);
+                if machine.joltage.iter().zip(&new_joltage).any(|(m, n)| n > m) {
+                    continue;
+                }
+                let cost = presses + p + total_joltage - new_joltage.iter().sum::<u32>();
+                states
+                    .entry(cost)
+                    .or_default()
+                    .push((presses + p, new_joltage));
+            }
+        }
     };
 
     min_presses
@@ -87,14 +141,26 @@ pub fn puzzle_1(input: &str) -> String {
 
     let res: usize = input
         .iter()
-        .map(|machine| search_buttons_press(machine))
+        .map(|machine| search_buttons_for_lights(machine))
         .sum();
 
     res.to_string()
 }
 
 pub fn puzzle_2(input: &str) -> String {
-    todo!()
+    let input = load_input(input);
+
+    let mut i = 0;
+    let res: u32 = input
+        .iter()
+        .inspect(|_| {
+            println!("{}", i);
+            i += 1;
+        })
+        .map(|machine| search_buttons_for_joltage(machine))
+        .sum();
+
+    res.to_string()
 }
 
 #[cfg(test)]
@@ -111,8 +177,8 @@ mod tests {
         assert_eq!(puzzle_1(INPUT), "7");
     }
 
-    // #[test]
-    // fn test_puzzle_2() {
-    //     assert_eq!(puzzle_2(INPUT), "");
-    // }
+    #[test]
+    fn test_puzzle_2() {
+        assert_eq!(puzzle_2(INPUT), "33");
+    }
 }
